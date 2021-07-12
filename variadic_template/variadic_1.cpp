a373e1a41b64014d66f1afcc0d152d9333e93a3e
amirsalarpd74 <amirsalar.p.d74@gmail.com>
2021-07-12 23:10:08 +0430

    Add variadic template practice.

---
+++ variadic_template/variadic_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <typename N>
+auto summer(N n)
+{
+    return n;
+}
+
+template <typename N, typename... Args>
+auto summer(N n, Args... args)
+{
+    return n + summer(args...);
+}
+
+int main()
+{
+    std::cout<<"add: "<< summer(4, 34.1, 13, 14, 15, 20) << std::endl;
+    std::cout<<"add: "<< summer(4, 34, 13, 14, 15, 20) << std::endl;
+}
