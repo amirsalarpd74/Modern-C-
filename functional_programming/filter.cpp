4dba5d01879459314658622b99610f7ac9c96340
amirsalarpd74 <amirsalar.p.d74@gmail.com>
2021-07-17 10:13:38 +0430

    Fix bug in variadic template example.

---
+++ functional_programming/filter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <ranges>
+
+using namespace std;
+
+int main()
+{
+    auto render = [](auto collection)
+    {
+        for (const auto& val : collection)
+            std::cout << val << std::endl;
+    };
+
+    vector<int> in_collection{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> out_collection;
+
+    std::transform(in_collection.begin(), in_collection.end(), back_inserter(out_collection), 
+         [](const int& val){ return val * 3;});
+
+    for (const int& val : in_collection | std::ranges::transform_view([](const int& vals){ return vals * 3;}))
+    {
+        printf("s %d \n", val);
+    }
+    render(out_collection);
+}
--- variadic_template/variadic_1.cpp
+++ variadic_template/variadic_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 template <typename N>
 auto summer(N n)
